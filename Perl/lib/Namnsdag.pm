7643592cd74002329378dc70564ca0f93a4155e2
Jan Lindblom <lindblom.jan@gmail.com>
2011-12-20 20:45:34 +0100

    Perl skeleton in place.

--- Perl/lib/Namnsdag.pm
+++ Perl/lib/Namnsdag.pm
@@ -1,17 +1,96 @@
 package Namnsdag;
 
-use strict;
 use warnings;
+use strict;
 
-use DBI;
-use DynaLoader();
+=head1 NAME
 
-use vars qw($VERSION @ISA);
+Namnsdag - The Perl Namnsdag library.
 
-BEGIN {
-    $VERSION = '1.0';
-    @ISA     = 'DynaLoader';
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Namnsdag retrieves the namnsdag of a given day.
+
+    use Namnsdag;
+
+    my $foo = Namnsdag->new();
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 namnsdag
+
+=cut
+
+sub namnsdag {
 }
 
-__PACKAGE__->bootstrap($VERSION);
+=head1 AUTHOR
 
+Jan Lindblom, C<< <lindblom.jan at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-namnsdag at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Namnsdag>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Namnsdag
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Namnsdag>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Namnsdag>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Namnsdag>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Namnsdag/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 Jan Lindblom, all rights reserved.
+
+This program is released under the following license: BSD
+
+
+=cut
+
+1; # End of Namnsdag
